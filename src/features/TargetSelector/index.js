02a9489a358a083717a2e10a877ceebfbbdb43f0
StrixROX <40167899+StrixROX@users.noreply.github.com>
2023-10-15 18:41:40 +0530

    TargetSelector now fetches data from API (avatars-only)

--- src/features/TargetSelector/index.js
+++ src/features/TargetSelector/index.js
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+'use client'
+
+import { useState, useEffect } from 'react'
 
 const BASE_URL_IMAGE = process.env.NEXT_PUBLIC_BASE_URL_IMAGE
 
@@ -15,49 +17,26 @@ function Icon({ data, isSelected, clickHandler }) {
       border: (isSelected() ? "2px solid black": "none")
     }}>
       <img src={BASE_URL_IMAGE + data.icon + '.png'} alt={data.name} style={{'width': "100%"}} />
-      <span className="name">{data.name}</span>
+      <span className="name">{ data.name + ` (${data.element})`}</span>
     </div>
   )
 }
 
 export default function TargetSelector({ onSubmit }) {
-  // sample data from db
-  const targets = {
-    "10000002": {
-      "_id": {
-        "$oid": "652a3f04fcc838608c413c7a"
-      },
-      "id": 10000002,
-      "rank": 5,
-      "name": "Kamisato Ayaka",
-      "element": "Ice",
-      "weaponType": "WEAPON_SWORD_ONE_HAND",
-      "icon": "UI_AvatarIcon_Ayaka",
-      "birthday": [
-        9,
-        28
-      ],
-      "release": 1626814800,
-      "route": "Kamisato Ayaka"
-    },
-    "10000003": {
-      "_id": {
-        "$oid": "652a3f04fcc838608c413c7b"
-      },
-      "id": 10000003,
-      "rank": 5,
-      "name": "Jean",
-      "element": "Wind",
-      "weaponType": "WEAPON_SWORD_ONE_HAND",
-      "icon": "UI_AvatarIcon_Qin",
-      "birthday": [
-        3,
-        14
-      ],
-      "release": 1600131600,
-      "route": "Jean"
-    }
-  } // { id1: {..., "id": id1, ...}, ... }
+  const [targets, setTargets] = useState({}) // { id1: {..., "id": id1, ...}, ... }
+  useEffect(() => {
+    fetch('/api/avatar')
+    .then(res => res.json())
+    .then(res => {
+      const formattedData = {}
+
+      for (let i in res) {
+        formattedData[res[i].id] = res[i]
+      }
+
+      setTargets(formattedData)
+    })
+  }, [])
 
   const [selected, setSelected] = useState([]) // [ id1, id2, ... ]
 
