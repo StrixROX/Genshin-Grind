7363b028af036639333d074e0140800cad52e09e
StrixROX <40167899+StrixROX@users.noreply.github.com>
2023-10-16 00:41:56 +0530

    Restructured TargetSelector feature into separate components

---
+++ src/features/TargetSelector/components/WeaponIcon.js
@@ -0,0 +1,19 @@
+const BASE_URL_IMAGE = process.env.NEXT_PUBLIC_BASE_URL_IMAGE
+
+export default function WeaponIcon({ data, isSelected, clickHandler }) {
+  return (
+    <div className="target" onClick={() => clickHandler(data)}
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      width: 100,
+      alignItems: 'center',
+      textAlign: 'center',
+      margin: 10,
+      border: (isSelected() ? "2px solid black": "none")
+    }}>
+      <img src={BASE_URL_IMAGE + data.icon + '.png'} alt={data.name} style={{'width': "100%"}} />
+      <span className="name">{ data.name }</span>
+    </div>
+  )
+}
