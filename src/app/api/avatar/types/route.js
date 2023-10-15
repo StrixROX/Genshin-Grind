c0d4faa9870f8168f36f48ad006d76b99c30139a
StrixROX <40167899+StrixROX@users.noreply.github.com>
2023-10-15 18:38:56 +0530

    Added API routes for fetching data from MongoDB

---
+++ src/app/api/avatar/types/route.js
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server'
+import clientPromise from '@lib/mongodb'
+
+export const GET = async () => {
+  const client = await clientPromise
+  const db = await client.db("genshindb")
+  const avatars = await db.collection("avatar")
+
+  return NextResponse.json(await avatars.find().filter({types: {$exists: true}}).toArray())
+}
